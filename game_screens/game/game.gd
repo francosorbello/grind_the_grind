66dbc4e368090dfb87a15808d3cbd2318e46d0cb
francosorbello <franco.sorbelo@gmail.com>
2025-08-02 18:35:53 -0300

    removed unused stuff

--- game_screens/game/game.gd
+++ game_screens/game/game.gd
@@ -8,8 +8,12 @@ func lose_game() -> void:
     await get_tree().create_timer(3.0).timeout
     if lose_screen:
         IndieBlueprintSceneTransitioner.transition_to(
-		lose_screen,
-		IndieBlueprintPremadeTransitions.Voronoi,
-		IndieBlueprintPremadeTransitions.Voronoi
-	)
+        lose_screen,
+        IndieBlueprintPremadeTransitions.Voronoi,
+        IndieBlueprintPremadeTransitions.Voronoi
+    )
     pass
+
+func _process(_delta):
+    if Input.is_action_just_released("bail"):
+        lose_game()
