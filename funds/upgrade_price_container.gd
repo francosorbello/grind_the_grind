60cacbcfab8b655bbe5cc2181737cd4b7c53a12d
francosorbello <franco.sorbelo@gmail.com>
2025-08-02 19:01:58 -0300

    added basic funds. you cant buy stuff if u dont have money

---
+++ funds/upgrade_price_container.gd
@@ -0,0 +1,8 @@
+extends Resource
+class_name UpgradePriceContainer
+
+@export var prices : Dictionary[Global.StatType, int] = {
+    Global.StatType.GRIND: 25,
+    Global.StatType.SPEED: 100,
+    Global.StatType.TRICK: 25,
+}
--- ui/store/store_button.gd
+++ ui/store/store_button.gd
@@ -2,32 +2,42 @@ extends Control
 
 @export var stat_type : Global.StatType
 
+@export_group("Funds")
+@export var prices_container : UpgradePriceContainer
+@export var funds : PointValue
+
 @export_group("Resources")
 @export var icons : Dictionary[Global.StatType, Texture2D]
 
 @onready var button : Button = $Button
 
 var titles : Dictionary[Global.StatType, String] = {
-    Global.StatType.SPEED: "Speed",
-    Global.StatType.GRIND: "Grind",
-    Global.StatType.TRICK: "Trick",
+	Global.StatType.SPEED: "Speed",
+	Global.StatType.GRIND: "Grind",
+	Global.StatType.TRICK: "Trick",
 }
 
 var hints : Dictionary[Global.StatType, String] = {
-    Global.StatType.SPEED: "Increase the speed you gain points while grinding.",
-    Global.StatType.GRIND: "Increase the amount of points obtained while grinding.",
-    Global.StatType.TRICK: "Increase the amount of points obtained by doing tricks.",
+	Global.StatType.SPEED: "Increase the speed you gain points while grinding.",
+	Global.StatType.GRIND: "Increase the amount of points obtained while grinding.",
+	Global.StatType.TRICK: "Increase the amount of points obtained by doing tricks.",
 }
 
 signal on_store_button_pressed(stat_type: Global.StatType)
 
 func _ready():
-    setup()
+	setup()
 
 func setup():
-    button.icon = icons.get(stat_type, null)
-    button.text = "Upgrade" + " " + titles.get(stat_type, "Unknown")
-    button.tooltip_text = hints.get(stat_type, "No hint available.")
+	button.icon = icons.get(stat_type, null)
+	button.text = "Upgrade" + " " + titles.get(stat_type, "Unknown") + "($%d)" % prices_container.prices.get(stat_type, 0)
+	button.tooltip_text = hints.get(stat_type, "No hint available.")
 
 func _on_button_pressed() -> void:
-    on_store_button_pressed.emit(stat_type)
+	on_store_button_pressed.emit(stat_type)
+
+func _process(_delta: float) -> void:
+	if funds.value < prices_container.prices.get(stat_type, 0):
+		button.disabled = true
+	else:
+		button.disabled = false
